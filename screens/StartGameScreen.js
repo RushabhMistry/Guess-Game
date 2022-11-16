0ada012787e3200909b421ee8b582d919f460f22
RushabhMistry <mistrynitinrushabh@gmail,com>
2022-11-16 18:46:02 +0530

    first commit

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,11 @@
+import { TextInput, View } from "react-native";
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TExtInput />
+    </View>
+  );
+}
+
+export default StartGameScreen;
